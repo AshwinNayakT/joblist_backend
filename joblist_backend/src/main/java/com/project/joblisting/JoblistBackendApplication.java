1718194027c5b22c98ef01a543be053b9ea7ea5c
AshwinNayakT <tanayak.ece@gmail.com>
2024-01-15 13:24:41 +0530

    Initial commit

---
+++ joblist_backend/src/main/java/com/project/joblisting/JoblistBackendApplication.java
@@ -0,0 +1,13 @@
+package com.project.joblisting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoblistBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoblistBackendApplication.class, args);
+	}
+
+}
---
+++ joblist_backend/src/test/java/com/project/joblisting/JoblistBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.joblisting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoblistBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
